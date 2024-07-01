f8607e00997eca21f67be14f68346e30ccebb068
Juan Vazquez <diclonius32@gmail.com>
2024-07-01 15:32:20 -0600

    + Screen Looper

---
+++ JetFighter/Assets/_Core/Scripts/General/ScreenLooper.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenLooper : MonoBehaviour
+{
+    public void WrapObject(Transform objTransform)
+    {
+        Vector3 newPosition = objTransform.position;
+    
+        float screenWidth = CameraManager.Ins.cam.orthographicSize * CameraManager.Ins.cam.aspect;
+        float screenHeight = CameraManager.Ins.cam.orthographicSize;
+    
+        if (objTransform.position.x > screenWidth)
+        {
+            newPosition.x = -screenWidth;
+        }
+        else if (objTransform.position.x < -screenWidth)
+        {
+            newPosition.x = screenWidth;
+        }
+    
+        if (objTransform.position.y > screenHeight)
+        {
+            newPosition.y = -screenHeight;
+        }
+        else if (objTransform.position.y < -screenHeight)
+        {
+            newPosition.y = screenHeight;
+        }
+    
+        objTransform.position = newPosition;
+    }
+}
