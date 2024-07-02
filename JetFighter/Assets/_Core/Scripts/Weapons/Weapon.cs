799baa949c575bdfdf239017e3bc051ce6a10c73
Juan Vazquez <diclonius32@gmail.com>
2024-07-02 11:11:35 -0600

    + Weapon class added.

--- JetFighter/Assets/_Core/Scripts/Weapons/Weapon.cs
+++ JetFighter/Assets/_Core/Scripts/Weapons/Weapon.cs
@@ -1,18 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.Pool;
 
-public class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private int fireRate;
+    private bool canShoot = true;
+
+    private ObjectPool<Projectile> projectilePool;
+
+    private void Awake()
     {
+        projectilePool = new ObjectPool<Projectile>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject);
+    }
+    public abstract void Shoot();
+
+    public void TryShoot()
+    {
+        if (!canShoot) { return; }
         
+        Shoot();
+        CanShootAgain();
+    }
+    
+    private async void CanShootAgain()
+    {
+        canShoot = false;
+        await UniTask.Delay(fireRate);
+        canShoot = true; 
+    }
+    
+    // * =====================================================================================================================================
+    // * 
+
+    protected Projectile GetProjectileFromPool()
+    {
+        return projectilePool.Get();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected void ReturnProjectileToPool(Projectile projectile)
     {
-        
+        projectilePool.Release(projectile);
+    }
+    
+    // * =====================================================================================================================================
+    // * POOL
+    
+    private Projectile CreatePooledItem()
+    {
+        GameObject bulletObject = Instantiate(bulletPrefab);
+        return bulletObject.GetComponent<Projectile>();
+    }
+
+    private void OnTakeFromPool(Projectile projectile)
+    {
+        projectile.gameObject.SetActive(true);
+    }
+
+    private void OnReturnedToPool(Projectile projectile)
+    {
+        projectile.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyPoolObject(Projectile projectile)
+    {
+        Destroy(projectile.gameObject);
     }
 }
