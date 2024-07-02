472bc9ac6cb026764fee2c9d72d62c2158348a7a
Juan Vazquez <diclonius32@gmail.com>
2024-07-02 13:00:09 -0600

    * Player SO Variables added.

---
+++ JetFighter/Assets/_Core/Scripts/SO/PlayerVariable.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using Obvious.Soap;
+
+[CreateAssetMenu(fileName = "scriptable_variable_" + nameof(Player), menuName = "Soap/ScriptableVariables/"+ nameof(Player))]
+public class PlayerVariable : ScriptableVariable<Player>
+{
+        
+}
+
