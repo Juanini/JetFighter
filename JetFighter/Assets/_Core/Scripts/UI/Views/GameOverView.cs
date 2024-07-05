260f86a6d22842549a59e255ebf802aa2439fa75
Juan Vazquez <diclonius32@gmail.com>
2024-07-04 21:19:46 -0600

    * Game Over View improvements

--- JetFighter/Assets/_Core/Scripts/UI/Views/GameOverView.cs
+++ JetFighter/Assets/_Core/Scripts/UI/Views/GameOverView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using HannieEcho.UI;
 using Obvious.Soap;
 using UnityEngine;
@@ -12,11 +13,28 @@ public class GameOverView : UIView
     [SerializeField] private Button changeGameModeButton;
     [SerializeField] private ScriptableEventNoParam onRematch;
 
+    [SerializeField] private GameObject buttonsContainer;
+    [SerializeField] private GameObject buttonsContainerOutPos;
+    [SerializeField] private GameObject buttonsContainerInPos;
+
     public override void OnViewCreated()
     {
         base.OnViewCreated();
         rematchButton.onClick.AddListener(OnRematchClick);
         changeGameModeButton.onClick.AddListener(OnChangeGameModeClick);
+    }
+
+    public override void OnViewBeforeAppear()
+    {
+        base.OnViewBeforeAppear();
+
+        buttonsContainer.transform.position = buttonsContainerOutPos.transform.position;
+    }
+
+    public override void OnViewAfterAppear()
+    {
+        base.OnViewAfterAppear();
+        ShowButtonsAnimated();
     }
 
     private void OnRematchClick()
@@ -29,5 +47,11 @@ public class GameOverView : UIView
     {
         navController.HideNavLastView();
         GameManager.Ins.TransitionToState(GameStates.ModeSelection);
+    }
+
+    private void ShowButtonsAnimated()
+    {
+        buttonsContainer.transform.DOMove(buttonsContainerInPos.transform.position, 0.25f)
+            .SetEase(Ease.OutQuad);
     }
 }
